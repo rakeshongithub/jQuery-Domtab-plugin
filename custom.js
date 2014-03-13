00e14c1a2cf27b4f4e8bae73d12c4773b4369ea0
rakeshongithub <rakeshpersonal@gmail.com>
2014-03-13 16:11:44 +0530

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,48 @@
+// Self Invoking function and making alies safe
+(function($, window, document, undefined){
+		  
+	var defaults = {
+			detailBox : $('.detail-tab-wrapper > div'), // Detail wrapper
+			activeClassName : 'active',                 // Class name for active tab
+			activeTab : 2,                              // Default active tab index value
+			events : 'mouseenter',                      // Events Mouseenter, click, hover
+			fadeInOut : true                            // Enable/Desable fadeIn/fadeOut effect by passing true and false.
+		};
+		
+	$.fn.domTab = function(options){
+		// grabing defaults value with the help of $.extend();
+		var settings = $.extend({}, defaults, options),
+			$this = $(this);
+		
+		// Set default active tab
+		$this.eq(settings.activeTab).addClass(settings.activeClassName)
+		
+		// find current active tab and it's realated detail box
+		$this.each(function(){
+			var $this = $(this), currentId;
+			if($this.is('.'+settings.activeClassName)){
+				currentId = $(this).index();
+				settings.detailBox.eq(currentId).addClass(settings.activeClassName).show()
+				.siblings().removeClass(settings.activeClassName).hide();
+			};
+		});
+		
+
+		//show/hide detail boxes onclick on tab list
+		$this.on(settings.events, function(){
+			var tabId = $(this).index();
+			$(this).addClass(settings.activeClassName)            // Add active click.
+    	    .siblings().removeClass(settings.activeClassName);    // Remove active class from other siblings.
+			
+			// Check if fadeIn/fadeOut effect is on or not
+			if(settings.fadeInOut){
+				settings.detailBox.eq(tabId).addClass(settings.activeClassName).fadeIn()             // fadeIn detail box.
+		        .siblings().removeClass(settings.activeClassName).fadeOut();                         // fadeOut other siblings detail box.
+			} else {
+				settings.detailBox.eq(tabId).addClass(settings.activeClassName).show()               // show detail box.
+		        .siblings().removeClass(settings.activeClassName).hide();                            // hide other siblings detail box.
+			};
+		});
+	};
+
+}(jQuery, window, document))
